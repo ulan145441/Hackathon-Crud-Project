ec9de211c10e3b6c950b6af7fa6473e41918c950
Ulan <105221955+ulan145441@users.noreply.github.com>
2022-07-06 15:30:41 +0600

    Hackthon
    kpi students

---
+++ js/script.js
@@ -0,0 +1,72 @@
+    let selectedRow = null
+
+function onFormSubmit(e) {
+	event.preventDefault();
+        let formData = readFormData();
+        if (selectedRow == null){
+            insertNewRecord(formData);
+		}
+        else{
+            updateRecord(formData);
+		}
+        resetForm();    
+}
+
+//Получить данные
+function readFormData() {
+    let formData = {};
+    formData["productCode"] = document.getElementById("productCode").value;
+    formData["product"] = document.getElementById("product").value;
+    formData["qty"] = document.getElementById("qty").value;
+    formData["perPrice"] = document.getElementById("perPrice").value;
+    return formData;
+}
+
+//Вставляем данные
+function insertNewRecord(data) {
+    let table = document.getElementById("storeList").getElementsByTagName('tbody')[0];
+    let newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+		cell1.innerHTML = data.productCode;
+    cell2 = newRow.insertCell(1);
+		cell2.innerHTML = data.product;
+    cell3 = newRow.insertCell(2);
+		cell3.innerHTML =   result = data.qty
+    cell4 = newRow.insertCell(3);
+		cell4.innerHTML = data.perPrice + data.qty * 22;
+    cell4 = newRow.insertCell(4);
+        cell4.innerHTML = `<button onClick="onEdit(this)">Edit</button> <button onClick="onDelete(this)">Delete</button>`;
+}
+
+//Редактируем данные
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("productCode").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("product").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("qty").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("perPrice").value = selectedRow.cells[3].innerHTML;
+}
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.productCode;
+    selectedRow.cells[1].innerHTML = formData.product;
+    selectedRow.cells[2].innerHTML = formData.qty;
+    selectedRow.cells[3].innerHTML = formData.perPrice;
+}
+
+//Удалить данные
+function onDelete(td) {
+    if (true    ) {
+        row = td.parentElement.parentElement;
+        document.getElementById('storeList').deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
+
+//Сброс данных
+function resetForm() {
+    document.getElementById("productCode").value = '';
+    document.getElementById("product").value = '';
+    document.getElementById("qty").value = '';
+    document.getElementById("perPrice").value = '';
+    selectedRow = null;
+}
